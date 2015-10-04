5f586331b96b391ed4cf7c2f95efe102c3fac70c
Minsheng Liu <lambda@liu.ms>
2015-10-04 14:31:33 -0700

    unification done (not tested)

--- src/Language/Indescript/TypeInference.hs
+++ src/Language/Indescript/TypeInference.hs
@@ -1,10 +1,16 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE FlexibleContexts    #-}
 
 module Language.Indescript.TypeInference where
 
 import Control.Monad.State
+import Control.Monad.Except
 import Data.Monoid ((<>))
-import Data.Map (Map, insert, (!))
+import           Data.Map.Strict (Map, (!))
+import qualified Data.Map.Strict as Map
+import qualified Data.List       as List
 
 import Language.Indescript.ADT
 
@@ -33,7 +39,7 @@ generateConstraints env (EApp f x) =
      return (ty, cs)
 generateConstraints env (EAbs v e) =
   do t1      <- nextTVar
-     (t2, c) <- generateConstraints (insert v t1 env) e
+     (t2, c) <- generateConstraints (Map.insert v t1 env) e
      return (TArr t1 t2, c)
 generateConstraints env (EIf x l r) =
   do (tx, cx) <- generateConstraints env x
@@ -43,7 +49,53 @@ generateConstraints env (EIf x l r) =
      return (tl, cs)
 generateConstraints env (ELet f e1 e2) =
   do tf       <- nextTVar
-     (t1, c1) <- generateConstraints (insert f tf env) e1
-     (t2, c2) <- generateConstraints (insert f tf env) e2
+     (t1, c1) <- generateConstraints (Map.insert f tf env) e1
+     (t2, c2) <- generateConstraints (Map.insert f tf env) e2
      let cs   =  c1 <> c2 <> [tf :== t1]
      return (t2, cs)
+
+type Substitution = Map TVar Type
+
+(|=>) :: TVar -> Type -> Substitution
+v |=> t = Map.singleton v t
+
+after :: Substitution -> Substitution -> Substitution
+after = undefined
+
+class Substitutable a where
+  apply    :: Substitution -> a -> a
+  freeVars :: a -> [TVar]
+
+instance Substitutable Constraints where
+  apply    = undefined
+  freeVars = undefined
+
+unify :: Constraints -> Substitution
+unify []              = Map.empty
+unify ((s :== t):cs') = case runExcept proc of
+    (Left  subst) -> subst
+    (Right msg)   -> error msg
+  where
+    sEqX (s' :== (TVar _)) = s' == s
+    sEqX _                 = False
+    xEqT ((TVar _) :== t') = t' == t
+    xEqT _                 = False
+
+    toArrow (TArr s1 s2) (TArr t1 t2) = Just (s1, s2, t1, t2)
+    toArrow _            _            = Nothing
+
+    tryBool :: (MonadError e m) => Bool -> e -> m ()
+    tryBool cond res = if cond then throwError res
+                   else return ()
+    tryMaybe :: (MonadError e m) => Maybe a -> (a -> e) -> m ()
+    tryMaybe (Just x) f = throwError $ f x
+    tryMaybe Nothing  _ = return ()
+    proc = do
+      tryBool (s == t) (unify cs')
+      tryMaybe (List.find sEqX cs') (\(_ :== (TVar x))
+        -> (unify $ apply (x |=> t) cs') `after` (x |=> t))
+      tryMaybe (List.find xEqT cs') (\((TVar x) :== _)
+        -> (unify $ apply (x |=> s) cs') `after` (x |=> s))
+      tryMaybe (toArrow s t) (\(s1, s2, t1, t2)
+        -> unify $ cs' <> [s1 :== t1, s2 :== t2])
+      return "unification failed"
