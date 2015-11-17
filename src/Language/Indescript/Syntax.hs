82903b151ad1fe5f1a185d6a89800b0a706d2479
Minsheng Liu <lambda@liu.ms>
2015-11-16 20:40:07 -0800

    Indexed recursion scheme!!!

--- src/Language/Indescript/Syntax.hs
+++ src/Language/Indescript/Syntax.hs
@@ -1,10 +1,51 @@
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE KindSignatures    #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE PolyKinds         #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Language.Indescript.Syntax where
 
+import Data.Traversable
+
+infixr 5 ~>
+type f ~> g = forall i. f i -> g i
+
+newtype K a b = K { unK :: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+newtype I a = I { unI :: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+instance Applicative I where
+  pure = I
+  I f <*> I a = I (f a)
+
+instance Monoid a => Applicative (K a) where
+  pure _ = K mempty
+  K a <*> K a' = K (mappend a a')
+
+class IxFunctor (f :: (k -> *) -> (k -> *)) where
+  imap :: (a ~> b) -> (f a ~> f b)
+
+class IxFunctor t => IxTraversable t where
+  itraverse :: Applicative f
+            => (forall i. a i -> f (b i))
+            -> (forall i. t a i -> f (t b i))
+
+imapDefault :: IxTraversable t => (a ~> b) -> (t a ~> t b)
+imapDefault f = unI . itraverse (I . f)
+
 newtype IxFix f i = In { out :: f (IxFix f) i }
+
+cata :: IxFunctor f => (f a ~> a) -> (IxFix f ~> a)
+cata phi = phi . imap (cata phi) . out
 
 data AstIx = AstDecl | AstExpr | AstPatt | AstType
 
@@ -19,7 +60,7 @@ data AstIxProxy (i :: AstIx) where
   PattProxy :: AstIxProxy AstPatt
   TypeProxy :: AstIxProxy AstType
 
-type AnnotAstF a f i = (a, AstF f i)
+newtype AnnotAstF a f i = Annot { unAnnot :: (a, AstF f i) }
 
 data AstF (f :: AstIx -> *) (i :: AstIx) where
   VarF   :: Var -> AstIxProxy i -> AstF f i
@@ -36,7 +77,7 @@ data AstF (f :: AstIx -> *) (i :: AstIx) where
   LetF   :: [f AstDecl] -> f AstExpr -> AstF f AstExpr
   CaseF  :: f AstExpr -> [Altn f] -> AstF f AstExpr
   TyGenF :: [f AstType] -> f AstType -> AstF f AstType
-  BindF  :: [f AstPatt] -> f AstPatt -> AstF f AstPatt
+  BindF  :: f AstPatt -> f AstPatt -> AstF f AstPatt
   HoleF  :: AstF f AstPatt
   EqtF   :: FnLhs f -> f AstExpr -> [f AstDecl] -> AstF f AstDecl
   EqtsF  :: [f AstDecl] -> AstF f AstDecl
@@ -45,6 +86,49 @@ data AstF (f :: AstIx -> *) (i :: AstIx) where
   TyAlsF :: f AstType -> f AstType -> AstF f AstDecl
   NewTyF :: f AstType -> f AstType -> f AstType -> AstF f AstDecl
   DatTyF :: f AstType -> [f AstType] -> AstF f AstDecl
+
+instance IxFunctor AstF where
+  imap = imapDefault
+
+instance IxFunctor (AnnotAstF a) where
+  imap = imapDefault
+
+instance IxTraversable AstF where
+  itraverse f = \case
+    (VarF var i)   -> VarF <$> pure var <*> pure i
+    (LitF lit)     -> LitF <$> pure lit
+    (ParenF e)     -> ParenF <$> f e
+    (AppF fn args) -> AppF <$> f fn <*> mapList f args
+    (InfixF l o r) -> InfixF <$> f l <*> f o <*> f r
+    (NegF e)       -> NegF <$> f e
+    (LOSecF o x)   -> LOSecF <$> f o <*> f x
+    (ROSecF o x)   -> LOSecF <$> f o <*> f x
+    (TyAnnF e t)   -> TyAnnF <$> f e <*> f t
+    (IfF e1 e2 e3) -> IfF <$> f e1 <*> f e2 <*> f e3
+    (LamF patts e) -> LamF <$> mapList f patts <*> f e
+    (LetF decls e) -> LetF <$> mapList f decls <*> f e
+    (CaseF e alts) -> CaseF <$> f e <*> alts'
+      where alts'        = mapList f' alts
+            f' (a, b, c) = (,,) <$> f a <*> f b <*> mapList f c
+    (TyGenF ts ty) -> TyGenF <$> mapList f ts <*> f ty
+    (BindF as src) -> BindF <$> f as <*> f src
+    (HoleF)        -> pure HoleF
+
+    (EqtF lhs e whs) -> let
+      lhs' = case lhs of
+        FnArgs fn xs -> FnArgs <$> f fn <*> mapList f xs
+        FnOp l o r   -> FnOp <$> f l <*> f o <*> f r
+      in EqtF <$> lhs' <*> f e <*> mapList f whs
+    (EqtsF eqts)     -> EqtsF <$> mapList f eqts
+    (OpFixF d l ops) -> OpFixF d l <$> mapList f ops
+    (TySigF xs ty)   -> TySigF <$> mapList f xs <*> f ty
+    (TyAlsF t' t)    -> TyAlsF <$> f t' <*> f t
+    (NewTyF t' c t)  -> NewTyF <$> f t' <*> f c <*> f t
+    (DatTyF t cs)    -> DatTyF <$> f t <*> mapList f cs
+    where mapList f xs = sequenceA $ map f xs
+
+instance IxTraversable (AnnotAstF x) where
+  itraverse f (Annot (x, t)) = (Annot . (x,)) <$> itraverse f t
 
 data Lit = LInt    Int
          | LFloat  Float
@@ -61,7 +145,10 @@ data Var = VarId  String
 
 type Altn  (f :: AstIx -> *) = (f AstPatt, f AstExpr, [f AstDecl])
 
-data FnLhs (f :: AstIx -> *) = FnArgs [f AstExpr] (f AstPatt)
-                                | FnOp   (f AstPatt) (f AstExpr) (f AstPatt)
+data FnLhs (f :: AstIx -> *) = FnArgs (f AstExpr) [f AstPatt]
+                             | FnOp   (f AstPatt) (f AstExpr) (f AstPatt)
 
 data AssocType = Infix | InfixL | InfixR deriving (Eq, Show)
+
+removeAnnot :: IxFix (AnnotAstF a) i -> IxFix AstF i
+removeAnnot = cata (In . snd . unAnnot)
