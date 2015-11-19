0feb160296150e19dcbf76fcd74c71fad906064e
Minsheng Liu <lambda@liu.ms>
2015-11-18 20:04:59 -0800

    refactor AST

---
+++ src/Control/IxFix.hs
@@ -0,0 +1,62 @@
+-- Thanks /u/AndrasKovacs for pointing out the indexed type families solution
+-- in typing ASt:
+--   https://goo.gl/77Hk6F
+--   https://goo.gl/cQECzb
+
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE PolyKinds         #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Control.IxFix where
+
+import Control.Monad
+
+--    # Helper Types and Functions
+--   ## Auxuiliaries
+infixr 5 ~>
+type f ~> g = forall i. f i -> g i
+
+newtype K a b = K { unK :: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+newtype I a = I { unI :: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+instance Applicative I where
+  pure = I
+  I f <*> I a = I (f a)
+
+instance Monoid a => Applicative (K a) where
+  pure _ = K mempty
+  K a <*> K a' = K (mappend a a')
+
+--   ## Indexed Type Classes
+class IxFunctor (f :: (k -> *) -> (k -> *)) where
+  imap :: (a ~> b) -> (f a ~> f b)
+
+imapDefault :: IxTraversable t => (a ~> b) -> (t a ~> t b)
+imapDefault f = unI . itraverse (I . f)
+
+-- TODO: use Template Haskell to automatically derive IxTraversable, with:
+--   * All non-indexed recursion should also be Traversable.
+--   * All indexed recursion should also be IxTraversable.
+class IxFunctor t => IxTraversable t where
+  itraverse :: Applicative f
+            => (forall i. a i -> f (b i))
+            -> (forall i. t a i -> f (t b i))
+
+--   ## Indexed Fixed-point Combinator
+newtype IxFix f i = In { out :: f (IxFix f) i }
+
+--    # Recursion Schemes
+--   ## Catamorphisms
+cata :: IxFunctor f => (f a ~> a) -> (IxFix f ~> a)
+cata phi = phi . imap (cata phi) . out
+
+cataM :: (Monad m, IxTraversable t)
+      => (forall i. t a i -> m (a i))
+      -> (forall i. IxFix t i -> m (a i))
+cataM phi = phi <=< itraverse (cataM phi) . out
--- src/Language/Indescript/Syntax.hs
+++ src/Language/Indescript/Syntax.hs
@@ -1,80 +1,39 @@
--- Thanks /u/AndrasKovacs for pointing out the index type families solution
--- https://www.reddit.com/r/haskell/comments/3sm1j1/how_to_mix_the_base_functorrecursion_scheme_stuff/cwyr61h
--- and his standalone example:
--- https://gist.github.com/AndrasKovacs/af856be6cf816e08da95
-
--- The code will be refactored soon.
-
-[/u/AndrasKovacs](
-
-{-# LANGUAGE GADTs             #-}
-{-# LANGUAGE KindSignatures    #-}
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE PolyKinds         #-}
-{-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE DeriveFoldable    #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase     #-}
+{-# LANGUAGE RankNTypes     #-}
+{-# LANGUAGE TupleSections  #-}
 
 module Language.Indescript.Syntax where
 
-import Data.Traversable
+import Control.IxFix
 
-infixr 5 ~>
-type f ~> g = forall i. f i -> g i
-
-newtype K a b = K { unK :: a }
-  deriving (Eq, Show, Functor, Foldable, Traversable)
-
-newtype I a = I { unI :: a }
-  deriving (Eq, Show, Functor, Foldable, Traversable)
-
-instance Applicative I where
-  pure = I
-  I f <*> I a = I (f a)
-
-instance Monoid a => Applicative (K a) where
-  pure _ = K mempty
-  K a <*> K a' = K (mappend a a')
-
-class IxFunctor (f :: (k -> *) -> (k -> *)) where
-  imap :: (a ~> b) -> (f a ~> f b)
-
-class IxFunctor t => IxTraversable t where
-  itraverse :: Applicative f
-            => (forall i. a i -> f (b i))
-            -> (forall i. t a i -> f (t b i))
-
-imapDefault :: IxTraversable t => (a ~> b) -> (t a ~> t b)
-imapDefault f = unI . itraverse (I . f)
-
-newtype IxFix f i = In { out :: f (IxFix f) i }
-
-cata :: IxFunctor f => (f a ~> a) -> (IxFix f ~> a)
-cata phi = phi . imap (cata phi) . out
-
+-- ####
+--    # Type Indexes and Proxies
 data AstIx = AstDecl | AstExpr | AstPatt | AstType
 
+--   ## Type Synonyms for Promoted Term Constrcutors
 type AstDecl = 'AstDecl
 type AstExpr = 'AstExpr
 type AstPatt = 'AstPatt
 type AstType = 'AstType
 
+--   ## Proxies
 data AstIxProxy (i :: AstIx) where
   DeclProxy :: AstIxProxy AstDecl
   ExprProxy :: AstIxProxy AstExpr
   PattProxy :: AstIxProxy AstPatt
   TypeProxy :: AstIxProxy AstType
 
-newtype AnnotAstF a f i = Annot { unAnnot :: (a, AstF f i) }
-
+--    # AST Base Functor
 data AstF (f :: AstIx -> *) (i :: AstIx) where
+--   ## Atomic AST Nodes (Expr, Patt, Decl)
   VarF   :: Var -> AstIxProxy i -> AstF f i
-  LitF   :: Lit -> AstF f AstExpr
+  ConF   :: Var -> AstIxProxy i -> AstF f i
+  LitF   :: Lit -> AstIxProxy i -> AstF f i
   ParenF :: f i -> AstF f i
+--   ## Complex AST Expression
   AppF   :: f i -> [f i] -> AstF f i
   InfixF :: f i -> f i -> f i -> AstF f i
   NegF   :: f AstExpr -> AstF f AstExpr
@@ -84,57 +43,64 @@ data AstF (f :: AstIx -> *) (i :: AstIx) where
   IfF    :: f AstExpr -> f AstExpr -> f AstExpr -> AstF f AstExpr
   LamF   :: [f AstPatt] -> f AstExpr -> AstF f AstExpr
   LetF   :: [f AstDecl] -> f AstExpr -> AstF f AstExpr
-  CaseF  :: f AstExpr -> [Altn f] -> AstF f AstExpr
+  AltF   :: f AstPatt -> f AstExpr -> [f AstDecl] -> AstF f AstExpr
+  CaseF  :: f AstExpr -> [f AstExpr] -> AstF f AstExpr
+--   ## Complex AST Type
+-- Generalized types, also known as “schemes”.
   TyGenF :: [f AstType] -> f AstType -> AstF f AstType
+--   ## Complex AST Pattern
   BindF  :: f AstPatt -> f AstPatt -> AstF f AstPatt
   HoleF  :: AstF f AstPatt
-  EqtF   :: FnLhs f -> f AstExpr -> [f AstDecl] -> AstF f AstDecl
+--   ## Complex AST Declarations
+-- Ordinary equation/function definitions.
+  EqtF   :: FnLhsF f AstExpr -> f AstExpr -> [f AstDecl] -> AstF f AstDecl
+-- Equation groups.
+-- Used both for multiple branch of a single function and binding groups during
+--   type inference in order to obtain most generalized types.
   EqtsF  :: [f AstDecl] -> AstF f AstDecl
+-- Operator fixity declarations.
+-- Removed after adjusting AST.
   OpFixF :: AssocType -> Int -> [f AstExpr] -> AstF f AstDecl
   TySigF :: [f AstExpr] -> f AstType -> AstF f AstDecl
   TyAlsF :: f AstType -> f AstType -> AstF f AstDecl
   NewTyF :: f AstType -> f AstType -> f AstType -> AstF f AstDecl
   DatTyF :: f AstType -> [f AstType] -> AstF f AstDecl
 
-instance IxFunctor AstF where
-  imap = imapDefault
+instance IxFunctor AstF where imap = imapDefault
 
-instance IxFunctor (AnnotAstF a) where
-  imap = imapDefault
-
+-- TODO: use Template Haskell to automatically derivie the following instance.
 instance IxTraversable AstF where
   itraverse f = \case
     (VarF var i)   -> VarF <$> pure var <*> pure i
-    (LitF lit)     -> LitF <$> pure lit
+    (ConF con i)   -> ConF <$> pure con <*> pure i
+    (LitF lit i)   -> LitF <$> pure lit <*> pure i
     (ParenF e)     -> ParenF <$> f e
-    (AppF fn args) -> AppF <$> f fn <*> mapList f args
+    (AppF fn args) -> AppF <$> f fn <*> traverse f args
     (InfixF l o r) -> InfixF <$> f l <*> f o <*> f r
     (NegF e)       -> NegF <$> f e
     (LOSecF o x)   -> LOSecF <$> f o <*> f x
     (ROSecF o x)   -> LOSecF <$> f o <*> f x
     (TyAnnF e t)   -> TyAnnF <$> f e <*> f t
     (IfF e1 e2 e3) -> IfF <$> f e1 <*> f e2 <*> f e3
-    (LamF patts e) -> LamF <$> mapList f patts <*> f e
-    (LetF decls e) -> LetF <$> mapList f decls <*> f e
-    (CaseF e alts) -> CaseF <$> f e <*> alts'
-      where alts'        = mapList f' alts
-            f' (a, b, c) = (,,) <$> f a <*> f b <*> mapList f c
-    (TyGenF ts ty) -> TyGenF <$> mapList f ts <*> f ty
+    (LamF patts e) -> LamF <$> traverse f patts <*> f e
+    (LetF decls e) -> LetF <$> traverse f decls <*> f e
+    (AltF p e ds)  -> AltF <$> f p <*> f e <*> traverse f ds
+    (CaseF e alts) -> CaseF <$> f e <*> traverse f alts
+    (TyGenF ts ty) -> TyGenF <$> traverse f ts <*> f ty
     (BindF as src) -> BindF <$> f as <*> f src
     (HoleF)        -> pure HoleF
-
-    (EqtF lhs e whs) -> let
-      lhs' = case lhs of
-        FnArgs fn xs -> FnArgs <$> f fn <*> mapList f xs
-        FnOp l o r   -> FnOp <$> f l <*> f o <*> f r
-      in EqtF <$> lhs' <*> f e <*> mapList f whs
-    (EqtsF eqts)     -> EqtsF <$> mapList f eqts
-    (OpFixF d l ops) -> OpFixF d l <$> mapList f ops
-    (TySigF xs ty)   -> TySigF <$> mapList f xs <*> f ty
+-- Declarations
+    (EqtF lhs e whs) -> EqtF <$> itraverse f lhs <*> f e <*> traverse f whs
+    (EqtsF eqts)     -> EqtsF <$> traverse f eqts
+    (OpFixF d l ops) -> OpFixF d l <$> traverse f ops
+    (TySigF xs ty)   -> TySigF <$> traverse f xs <*> f ty
     (TyAlsF t' t)    -> TyAlsF <$> f t' <*> f t
     (NewTyF t' c t)  -> NewTyF <$> f t' <*> f c <*> f t
-    (DatTyF t cs)    -> DatTyF <$> f t <*> mapList f cs
-    where mapList f xs = sequenceA $ map f xs
+    (DatTyF t cs)    -> DatTyF <$> f t <*> traverse f cs
+
+newtype AnnotAstF a f i = Annot { unAnnot :: (a, AstF f i) }
+
+instance IxFunctor (AnnotAstF a) where imap = imapDefault
 
 instance IxTraversable (AnnotAstF x) where
   itraverse f (Annot (x, t)) = (Annot . (x,)) <$> itraverse f t
@@ -152,12 +118,33 @@ data Var = VarId  String
          | ConSym String
          deriving (Eq, Show)
 
-type Altn  (f :: AstIx -> *) = (f AstPatt, f AstExpr, [f AstDecl])
-
-data FnLhs (f :: AstIx -> *) = FnArgs (f AstExpr) [f AstPatt]
-                             | FnOp   (f AstPatt) (f AstExpr) (f AstPatt)
-
 data AssocType = Infix | InfixL | InfixR deriving (Eq, Show)
+
+data FnLhsF (f :: AstIx -> *) (i :: AstIx) where
+  FnArgsF :: f AstExpr -> [f AstExpr] -> FnLhsF f AstExpr
+  FnOpF   :: f AstExpr -> f AstExpr -> f AstExpr -> FnLhsF f AstExpr
+
+instance IxFunctor FnLhsF where imap = imapDefault
+
+-- TODO: use Template Haskell to automatically derivie the following instance.
+instance IxTraversable FnLhsF where
+  itraverse f = \case
+    FnArgsF fn xs -> FnArgsF <$> f fn <*> traverse f xs
+    FnOpF   l o r -> FnOpF <$> f l <*> f o <*> f r
 
 removeAnnot :: IxFix (AnnotAstF a) i -> IxFix AstF i
 removeAnnot = cata (In . snd . unAnnot)
+
+{-
+collectBoundNames :: IxFix (AnnotAstF ElemPos) i
+                  -> IxFix (AnnotAstF _) i
+collectBoundNames = cata (In . collect . unAnnot) where
+  collect (pos, ast) = (\case
+    e@(LamF patts _) -> let bns   = map boundNames patts
+                            annt' = SomeCon bns pos
+                        in Annot (annt', e)
+    e@(LetF decls _) -> let bns   = map boundNames decls
+                            annt' = SomeCon bns pos
+                        in Annot (annt', e)
+    ) ast
+-}
